5d47b5926a38aec96b35418c6e3d903bf3386806
RUDI SOPIAN <sopian.rudi@gmail.com>
2020-11-02 23:28:53 +0700

    firstinit

---
+++ app/src/main/java/com/example/customlistview/Movie.java
@@ -0,0 +1,27 @@
+package com.example.customlistview;
+
+public class Movie {
+    public String title;
+    public String year;
+
+    public Movie(String title, String year) {
+        this.title = title;
+        this.year = year;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getYear() {
+        return year;
+    }
+
+    public void setYear(String year) {
+        this.year = year;
+    }
+}
